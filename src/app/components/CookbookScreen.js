6a77e155bc0e70a68386c1285b55da43a364349d
Jhia Turner <jhimitu@gmail.com>
2021-08-20 21:11:20 -0700

    list recipes in cookbook view

--- src/app/components/CookbookScreen.js
+++ src/app/components/CookbookScreen.js
@@ -1,11 +1,85 @@
-import React from 'react';
-import {Button, View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {Button, View, Text, TouchableOpacity} from 'react-native';
+import {FlatList} from 'react-native-gesture-handler';
 import {globalStyles} from '../styles/global';
 
 const CookbookScreen = ({navigation}) => {
+  const [recipes, setRecipes] = useState([
+    {
+      title: 'picadillo',
+      servings: 4,
+      ingredients: [{amount: '1 dum', ingredient: 'dummy ingredient'}],
+      methods: ['dummy method'],
+      materials: ['dummy material'],
+      favorite: true,
+      recipeImg: 'dummyImgURL',
+      recipeId: '1',
+    },
+    {
+      title: 'black eyed peas & turkey necks',
+      servings: 4,
+      ingredients: [{amount: '1 dum', ingredient: 'dummy ingredient'}],
+      methods: ['dummy method'],
+      materials: ['dummy material'],
+      favorite: true,
+      recipeImg: 'dummyImgURL',
+      recipeId: '2',
+    },
+    {
+      title: 'peri peri chicken',
+      servings: 4,
+      ingredients: [{amount: '1 dum', ingredient: 'dummy ingredient'}],
+      methods: ['dummy method'],
+      materials: ['dummy material'],
+      favorite: true,
+      recipeImg: 'dummyImgURL',
+      recipeId: '3',
+    },
+    {
+      title: 'camarones a la criolla',
+      servings: 4,
+      ingredients: [{amount: '1 dum', ingredient: 'dummy ingredient'}],
+      methods: ['dummy method'],
+      materials: ['dummy material'],
+      favorite: true,
+      recipeImg: 'dummyImgURL',
+      recipeId: '4',
+    },
+    {
+      title: 'oxtail stew',
+      servings: 4,
+      ingredients: [{amount: '1 dum', ingredient: 'dummy ingredient'}],
+      methods: ['dummy method'],
+      materials: ['dummy material'],
+      favorite: true,
+      recipeImg: 'dummyImgURL',
+      recipeId: '5',
+    },
+    {
+      title: 'cuban black beans',
+      servings: 4,
+      ingredients: [{amount: '1 dum', ingredient: 'dummy ingredient'}],
+      methods: ['dummy method'],
+      materials: ['dummy material'],
+      favorite: true,
+      recipeImg: 'dummyImgURL',
+      recipeId: '6',
+    },
+  ]);
+
   return (
     <View style={globalStyles.container}>
       <Text style={globalStyles.titleText}>My Cookbook</Text>
+      <FlatList
+        data={recipes}
+        keyExtractor={item => item.recipeId}
+        renderItem={({item}) => (
+          <TouchableOpacity
+            onPress={() => navigation.navigate('RecipeScreen', item)}>
+            <Text style={globalStyles.titleText}>{item.title}</Text>
+          </TouchableOpacity>
+        )}
+      />
       <Button title="Home" onPress={() => navigation.navigate('HomeScreen')} />
       <Button
         title="AddRecipe"
--- src/app/navigation/navigation.js
+++ src/app/navigation/navigation.js
@@ -16,7 +16,11 @@ const Navigator = () => {
         component={HomeScreen}
         options={{title: 'Welcome to Cookbük'}}
       />
-      <Stack.Screen name="CookbookScreen" component={CookbookScreen} />
+      <Stack.Screen
+        name="CookbookScreen"
+        component={CookbookScreen}
+        options={{title: 'My Cookbook'}}
+      />
       <Stack.Screen name="RecipeScreen" component={RecipeScreen} />
       <Stack.Screen
         name="AddRecipeFormScreen"
